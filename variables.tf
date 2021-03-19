254762b7675789d9a2bc20fd9dc6f0a0227332e8
Kristian Decker <kris@codecker.com>
2021-03-18 23:29:54 -0400

    Updated comments in variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,14 @@
+# Provide the name of your security group
 variable "sg_name" {}
 
+# Provide the destination VPC for the security group 
 variable "vpc" {}
 
+# Provide a description for your security group
 variable "sg_description" {}
 
+# sg_rules should be a map type of the required security group rules
+# see example in readme.md
 variable "sg_rules" {
   type = map 
 }
